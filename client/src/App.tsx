55d2f03c1e9bd02707ea44c79d02edeb20becd85
salimraza786 <127436003+salimraza786@users.noreply.github.com>
2025-01-11 13:00:15 +0530

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import Home from "./pages/Home"
-import Signup from "@/pages/Signup"
-import Login from "@/pages/login"
+import Signup from "./pages/Signup"
+import Login from "./pages/Login"
 
 function App() {
   const router = createBrowserRouter([
